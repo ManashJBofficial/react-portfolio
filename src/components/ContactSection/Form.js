fca0931fc574aa6e507d1b21c119d2631a9ea86c
Manash Jyoti Baruah <51418862+ManashJBofficial@users.noreply.github.com>
2023-12-10 00:00:01 +0530

    emailjs form update

--- src/components/ContactSection/Form.js
+++ src/components/ContactSection/Form.js
@@ -1,53 +1,54 @@
-import React from "react";
-import { Button } from "./ButtonElement2.js";
-// import emailjs from 'emailjs-com'
-// import toast, { Toaster } from "react-hot-toast";
-import { NameInput, EmailInput, MessageInput } from "./FormStyle.js";
+import React from 'react'
+import {Button} from './ButtonElement2.js'
+import emailjs from 'emailjs-com'
+import toast, { Toaster } from 'react-hot-toast';
+import {
+    NameInput,
+    EmailInput,
+    MessageInput,
+} from './FormStyle.js'
+
 
 const Form = () => {
-  // const notify = () => toast.success('Mail Sent Successfully !');
-  // function sendEmail(e){
-  //     e.preventDefault();
+    const notify = () => toast.success('Mail Sent Successfully !');
+    function sendEmail(e){
+        e.preventDefault();
 
-  //     emailjs.sendForm('gmail', 'template_id_mjb', e.target, 'user_6NVfcYOPSNthhz4JmL3fC')
-  //     .then((result) => {
-  //         console.log(result.text);
-  //         // alert('Message Sent Successfully !');
-  //         notify();
-  //     }, (error) => {
-  //         console.log(error.text);
-  //     });
-  //     e.target.reset();
+        emailjs.sendForm('service_6738c6v', 'template_t4p2ve6', e.target, 'nr-q5xMI14SDqR-ZJ')
+        .then((result) => {
+            console.log(result.text);
+            // alert('Message Sent Successfully !');
+            notify();
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset();
+        
+    }
+    return (
+        <div>
+                <form  onSubmit={sendEmail}>
+                    <NameInput>
+                        <label htmlFor="nameInput">Name</label>
+                        <input type="text" id="nameInput"   
+                        name="name" required></input>      
+                    </NameInput>
+                    <EmailInput>
+                        <label htmlFor="emailInput">Email</label>
+                        <input type="email" id="emailInput"  
+                        name="email" required></input>                                     
+                    </EmailInput>
+                    <MessageInput>
+                        <label htmlFor="messageInput">Message</label>
+                        <textarea type="text" id="messageInput"
+                        name="message" required></textarea>                                    
+                    </MessageInput>
+                    <Button type="submit" value="Send">Submit</Button>
+                    <Toaster position="bottom-right" reverseOrder={false}/>
+                </form>
+                
+        </div>
+    )
+}
 
-  // }
-  return (
-    <div>
-      <form name="contact_form" method="POST" netlify>
-        <input type="hidden" name="contact_form" value="contact_form" />
-        <NameInput>
-          <label htmlFor="nameInput">Name</label>
-          <input type="text" id="nameInput" name="name" required></input>
-        </NameInput>
-        <EmailInput>
-          <label htmlFor="emailInput">Email</label>
-          <input type="email" id="emailInput" name="email" required></input>
-        </EmailInput>
-        <MessageInput>
-          <label htmlFor="messageInput">Message</label>
-          <textarea
-            type="text"
-            id="messageInput"
-            name="message"
-            required
-          ></textarea>
-        </MessageInput>
-        <Button type="submit" value="Send">
-          Submit
-        </Button>
-        {/* <Toaster position="bottom-right" reverseOrder={false} /> */}
-      </form>
-    </div>
-  );
-};
-
-export default Form;
+export default Form
