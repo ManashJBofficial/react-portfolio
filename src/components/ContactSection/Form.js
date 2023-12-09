4896342f44a1be7c9dace68f32aedbfbee44bafa
Manash Jyoti Baruah <51418862+ManashJBofficial@users.noreply.github.com>
2023-12-09 13:06:34 +0530

    update netlify form

--- src/components/ContactSection/Form.js
+++ src/components/ContactSection/Form.js
@@ -1,54 +1,52 @@
-import React from 'react'
-import {Button} from './ButtonElement2.js'
-import emailjs from 'emailjs-com'
-import toast, { Toaster } from 'react-hot-toast';
-import {
-    NameInput,
-    EmailInput,
-    MessageInput,
-} from './FormStyle.js'
-
+import React from "react";
+import { Button } from "./ButtonElement2.js";
+// import emailjs from 'emailjs-com'
+// import toast, { Toaster } from "react-hot-toast";
+import { NameInput, EmailInput, MessageInput } from "./FormStyle.js";
 
 const Form = () => {
-    const notify = () => toast.success('Mail Sent Successfully !');
-    // function sendEmail(e){
-    //     e.preventDefault();
+  // const notify = () => toast.success('Mail Sent Successfully !');
+  // function sendEmail(e){
+  //     e.preventDefault();
 
-    //     emailjs.sendForm('gmail', 'template_id_mjb', e.target, 'user_6NVfcYOPSNthhz4JmL3fC')
-    //     .then((result) => {
-    //         console.log(result.text);
-    //         // alert('Message Sent Successfully !');
-    //         notify();
-    //     }, (error) => {
-    //         console.log(error.text);
-    //     });
-    //     e.target.reset();
-        
-    // }
-    return (
-        <div>
-                <form name="contact" method="POST" netlify>
-                    <NameInput>
-                        <label htmlFor="nameInput">Name</label>
-                        <input type="text" id="nameInput"   
-                        name="name" required></input>      
-                    </NameInput>
-                    <EmailInput>
-                        <label htmlFor="emailInput">Email</label>
-                        <input type="email" id="emailInput"  
-                        name="email" required></input>                                     
-                    </EmailInput>
-                    <MessageInput>
-                        <label htmlFor="messageInput">Message</label>
-                        <textarea type="text" id="messageInput"
-                        name="message" required></textarea>                                    
-                    </MessageInput>
-                    <Button type="submit" value="Send">Submit</Button>
-                    <Toaster position="bottom-right" reverseOrder={false}/>
-                </form>
-                
-        </div>
-    )
-}
+  //     emailjs.sendForm('gmail', 'template_id_mjb', e.target, 'user_6NVfcYOPSNthhz4JmL3fC')
+  //     .then((result) => {
+  //         console.log(result.text);
+  //         // alert('Message Sent Successfully !');
+  //         notify();
+  //     }, (error) => {
+  //         console.log(error.text);
+  //     });
+  //     e.target.reset();
 
-export default Form
+  // }
+  return (
+    <div>
+      <form name="contact" method="POST" netlify>
+        <NameInput>
+          <label htmlFor="nameInput">Name</label>
+          <input type="text" id="nameInput" name="name" required></input>
+        </NameInput>
+        <EmailInput>
+          <label htmlFor="emailInput">Email</label>
+          <input type="email" id="emailInput" name="email" required></input>
+        </EmailInput>
+        <MessageInput>
+          <label htmlFor="messageInput">Message</label>
+          <textarea
+            type="text"
+            id="messageInput"
+            name="message"
+            required
+          ></textarea>
+        </MessageInput>
+        <Button type="submit" value="Send">
+          Submit
+        </Button>
+        {/* <Toaster position="bottom-right" reverseOrder={false} /> */}
+      </form>
+    </div>
+  );
+};
+
+export default Form;
